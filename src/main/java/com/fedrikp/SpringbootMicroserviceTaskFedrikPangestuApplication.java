79b93a2ea8050629ef29690101b4650f2a0d6945
Fedrik Pangestu <fedrikpangestu1@gmail.com>
2026-06-10 17:35:09 +0700

    feat: initial project setup with Spring Boot 4.0.6

---
+++ src/main/java/com/fedrikp/SpringbootMicroserviceTaskFedrikPangestuApplication.java
@@ -0,0 +1,13 @@
+package com.fedrikp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceTaskFedrikPangestuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroserviceTaskFedrikPangestuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fedrikp/SpringbootMicroserviceTaskFedrikPangestuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedrikp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroserviceTaskFedrikPangestuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
