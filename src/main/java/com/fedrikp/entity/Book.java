80065316140510492f98b4326ab41df35e67dccd
Fedrik Pangestu <fedrikpangestu1@gmail.com>
2026-06-10 19:02:46 +0700

    feat: add Book and Category entity

---
+++ src/main/java/com/fedrikp/entity/Book.java
@@ -0,0 +1,98 @@
+package com.fedrikp.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(nullable = false)
+	private String author;
+	
+	@Column(nullable = false)
+	private String isbn;
+	
+	@Column(nullable = false, name = "published_date")
+	private LocalDate publishedDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+	
+	public Book() {}
+
+	public Book(Long id, String title, String author, String isbn, LocalDate publishedDate, Category category) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.publishedDate = publishedDate;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public LocalDate getPublishedDate() {
+		return publishedDate;
+	}
+
+	public void setPublishedDate(LocalDate publishedDate) {
+		this.publishedDate = publishedDate;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/fedrikp/entity/Category.java
@@ -0,0 +1,45 @@
+package com.fedrikp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true, name = "category_name")
+	private String categoryName;
+
+	public Category() {}
+	
+	public Category(Long id, String categoryName) {
+		this.id = id;
+		this.categoryName = categoryName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
+	
+}
